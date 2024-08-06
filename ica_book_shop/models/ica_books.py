e79153f88bfbcaf0ee9bd59c4639b3a118ce3f5a
Agga <agga@Aggas-MacBook-Air.local>
2024-08-06 22:15:25 +0700

    add readonly

--- ica_book_shop/models/ica_books.py
+++ ica_book_shop/models/ica_books.py
@@ -9,6 +9,9 @@ class IcaBooks(models.Model):
     release_year = fields.Integer()
     cover = fields.Binary()
     partner_id = fields.Many2one('res.partner', string="Author", required=True)
+    mobile = fields.Char(related="partner_id.mobile",readonly=True)
+    email = fields.Char(related="partner_id.email")
+    image_1920 = fields.Binary(related="partner_id.image_1920")
     category_ids = fields.Many2many('ica.book.category')
     state = fields.Selection(
         [
@@ -18,3 +21,19 @@ class IcaBooks(models.Model):
             ('cancel', 'Cancel'),
         ]
         , default='draft')
+
+    company_id = fields.Many2one('res.company', string="Company", default=lambda self: self.env.company)
+    currency_id = fields.Many2one('res.currency', string="Currency", related='company_id.currency_id')
+    sale_price = fields.Monetary(string="Sale Price")
+
+    def action_draft(self):
+        self.state = 'draft'
+
+    def action_available(self):
+        self.state = 'available'
+
+    def action_no_available(self):
+        self.state = 'no_available'
+
+    def action_cancel(self):
+        self.state = 'cancel'
